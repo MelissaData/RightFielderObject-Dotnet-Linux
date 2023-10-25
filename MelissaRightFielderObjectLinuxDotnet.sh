2b0479b2cf77d46dcb7fa5d983893e3bb4307c07
Melissa <>
2023-10-25 13:33:28 -0700

    Tested successfully using Melissa data files for 2023.10

--- MelissaRightFielderObjectLinuxDotnet.sh
+++ MelissaRightFielderObjectLinuxDotnet.sh
@@ -42,7 +42,7 @@ while [ $# -gt 0 ] ; do
 done
 
 # ######################### Config ###########################
-RELEASE_VERSION='2023.09'
+RELEASE_VERSION='2023.10'
 ProductName="RF_DATA"
 
 # Uses the location of the .sh file 
